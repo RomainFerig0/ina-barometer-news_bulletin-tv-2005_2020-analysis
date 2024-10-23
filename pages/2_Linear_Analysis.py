2378961d1cfcf42dbe64482afa9ffd5f655bed72
RomainFerig0 <104143932+RomainFerig0@users.noreply.github.com>
2024-10-23 11:10:06 +0200

    Update 2_Linear_Analysis.py

--- pages/2_Linear_Analysis.py
+++ pages/2_Linear_Analysis.py
@@ -10,7 +10,7 @@ from bokeh.plotting import figure
 
 #%% Extraction of the data
 
-data = pd.read_csv(r"C:\Users\papel\DATA\App\data\barometre_2005_2020.csv", delimiter=";", header=0, encoding='latin-1')
+data = pd.read_csv(r"data/barometre_2005_2020.csv", delimiter=";", header=0, encoding='latin-1')
 
 #%% Extraction of the "MOIS" data of object type, conversion to datetime, extraction of the month and year number. 
 
